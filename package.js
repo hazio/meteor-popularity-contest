9d9bf2c6964e46ad8b47c218a12c94dfd570ae3f
Jani Halmetoja <jani.halmetoja@gmail.com>
2015-04-10 16:53:12 +0300

    Added description

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'hazio:popularity-contest',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '0.0.2',
+  summary: 'Generates a popularity value of collection based on user defined fields & factors',
+  git: 'https://github.com/hazio/meteor-popularity-contest',
   documentation: 'README.md'
 });
 
